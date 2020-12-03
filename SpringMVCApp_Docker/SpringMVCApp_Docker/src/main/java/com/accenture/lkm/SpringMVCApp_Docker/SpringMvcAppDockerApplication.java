2900de08a6a306ae6f0ebd2a1c2229e9922e1edd
gitikab@yahoo.co.in <gitikab@yahoo.co.in>
2020-12-03 15:54:29 +0530

    first commit

---
+++ SpringMVCApp_Docker/SpringMVCApp_Docker/src/main/java/com/accenture/lkm/SpringMVCApp_Docker/SpringMvcAppDockerApplication.java
@@ -0,0 +1,13 @@
+package com.accenture.lkm.SpringMVCApp_Docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcAppDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcAppDockerApplication.class, args);
+	}
+
+}
---
+++ SpringMVCApp_Docker/SpringMVCApp_Docker/src/test/java/com/accenture/lkm/SpringMVCApp_Docker/SpringMvcAppDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.lkm.SpringMVCApp_Docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcAppDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
